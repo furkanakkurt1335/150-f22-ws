4f24cab582c2195826b0eeade699c209614fbb0a
Furkan Akkurt <71407287+furkanakkurt1335@users.noreply.github.com>
2022-10-16 18:12:54 +0300

    init repo with the first folder of the first question of the week of 17 Oct

---
+++ 10-17_ws2/cone/src/Main.py
@@ -0,0 +1,57 @@
+
+file_name = input()
+
+# Received r, h, cup_volume and glass_volume as floating point numbers from the user,
+# DO NOT write any prompt messages inside the input functions.
+#----------------------------------------------------------------
+# DO_NOT_EDIT_ANYTHING_ABOVE_THIS_LINE
+
+file = open(file_name)
+
+r = float(file.readline())
+h = float(file.readline())
+cup_volume = float(file.readline())
+glass_volume = float(file.readline())
+
+# DO_NOT_EDIT_ANYTHING_BELOW_THIS_LINE
+#----------------------------------------------------------------
+print("r: %.2f, h: %.2f" % (r, h))
+
+import math
+
+# Define the required functions in the following section.
+#----------------------------------------------------------------
+# DO_NOT_EDIT_ANYTHING_ABOVE_THIS_LINE
+
+def  calculate_cone_base_area():
+    return 3 * (r**2)
+
+def calculate_cone_lateral_area():
+    return 3 * r * math.sqrt(h**2 + r**2)
+
+def calculate_cone_volume():
+    return 3 * (r**2) * h / 3
+
+# DO_NOT_EDIT_ANYTHING_BELOW_THIS_LINE
+#----------------------------------------------------------------
+
+print("Cone's base area: %.2f" % (calculate_cone_base_area()))
+
+# display how many times we should use the cone to fill the cup fully
+#----------------------------------------------------------------
+# DO_NOT_EDIT_ANYTHING_ABOVE_THIS_LINE
+
+cone_volume = calculate_cone_volume()
+print(cup_volume / cone_volume)
+
+# DO_NOT_EDIT_ANYTHING_BELOW_THIS_LINE
+#----------------------------------------------------------------
+
+# display what percentage of the cone's volume will be empty, shown as integer
+#----------------------------------------------------------------
+# DO_NOT_EDIT_ANYTHING_ABOVE_THIS_LINE
+
+print(int(((cone_volume % glass_volume) / cone_volume) * 100))
+
+# DO_NOT_EDIT_ANYTHING_BELOW_THIS_LINE
+#----------------------------------------------------------------
---
+++ 10-17_ws2/cone/src/Tester.py
@@ -0,0 +1,196 @@
+
+# 20220601, author Murat Ozyurt
+# Question tester for pre-grading check during exam.
+# The tests feed the necessary input to your executable similar to the grading process.
+
+# Value of the "executable_file" variable must match to the specified executable in TCAdmin
+# Students or candidates should not modify this file, otherwise, compilation problems may occur during grading.
+
+# Instructors can add as many tests as they need.
+# In each testX() function, the test number, input lines and expected output lines must be returned.
+# Be careful with the use of """ at the end of input and expected output:
+# Terminating """ of the expected output has to be placed on a separate line,
+# whereas the terminating """ of the input has to be placed on the same line as the last input line.
+
+# Some students return the expected value without any calculation, so it is a better practice
+# to avoid using these test values among the actual grading test cases.  Modified versions of this file
+# can be bundled with the question for the students to use during exam and project development.
+
+"""
+https://stackoverflow.com/questions/13332268/how-to-use-subprocess-command-with-pipes
+
+ps = subprocess.Popen(('ps', '-A'), stdout=subprocess.PIPE)
+output = subprocess.check_output(('grep', 'process_name'), stdin=ps.stdout)
+ps.wait()
+"""
+
+import subprocess
+
+def runtest(executable_file, test_data):
+    # Instructors need not change this method, if you do please let us know.
+    # command = ["/usr/local/bin/python3", executable_file]
+    command = ["python", executable_file]
+
+
+    test_num = test_data[0]
+    input_to_test = test_data[1]
+    expected_output = test_data[2].rstrip()
+
+    p = subprocess.Popen(command, stdout=subprocess.PIPE, stdin=subprocess.PIPE)
+
+
+    #out, err = p.communicate(b"10\nsecondinput")
+    out, err = p.communicate(bytes(input_to_test.encode("utf-8")))
+    answer = out.decode("utf-8").rstrip()
+    print("_______________________________________________________")
+    if answer == expected_output:
+        print("| Test", test_num, ": SUCCESS.")
+        return
+    
+    test_fail_message = ""
+    test_success = True
+    
+    if answer.count("\n") != expected_output.count("\n"):
+        test_fail_message = "The number of generated answer output lines does not match that of the expected output."
+        test_success = False
+    
+    if test_success:
+        answer_lines = answer.split("\n")
+        expected_output_lines = expected_output.split("\n")
+
+        for lineNumber in range(len(answer_lines)):
+            if answer_lines[lineNumber].rstrip() != expected_output_lines[lineNumber].rstrip():
+                test_success = False
+                
+                result = " \""+answer_lines[lineNumber]+"\" does not match the expected output \""\
+                         +expected_output_lines[lineNumber]+"\""
+                test_fail_message += "\n" + "Answer line " +str(lineNumber+1)+ result
+
+
+    if test_success:
+        print("| Test", test_num, ": SUCCESS.")
+        return
+        
+    
+    print("Test", test_num, "FAILED.")
+    print("---------------------\nANSWER BEGIN\n---------------------")
+    print(answer)
+    print("---------------------\nANSWER END")
+    print("---------------------\nEXPECTED OUTPUT BEGIN\n---------------------")
+    print(expected_output)
+    print("---------------------\nEXPECTED OUTPUT END\n---------------------")
+    print(test_fail_message)
+
+
+def test1():
+
+    input_to_test = """\
+file1.txt"""
+
+    expected_output = """\
+r: 6.00, h: 6.00
+Cone's base area: 108.00
+5.0
+100
+"""
+    return 1, input_to_test, expected_output
+
+
+def test2():
+
+    input_to_test = """\
+file2.txt"""
+
+    expected_output = """\
+r: 3.00, h: 12.00
+Cone's base area: 27.00
+10.0
+100
+"""
+    return 2, input_to_test, expected_output
+
+def test3():
+
+    input_to_test = """\
+file3.txt"""
+
+    expected_output = """\
+r: 6.00, h: 5.00
+Cone's base area: 108.00
+8.0
+100
+"""
+    return 3, input_to_test, expected_output
+
+def testG1():
+
+    input_to_test = """\
+../testcases/file_1.txt"""
+
+    expected_output = """\
+r: 6.00, h: 2.00
+Cone's base area: 108.00
+10.0
+100
+"""
+    return 4, input_to_test, expected_output
+
+def testG2():
+
+    input_to_test = """\
+../testcases/file_2.txt"""
+
+    expected_output = """\
+r: 12.00, h: 7.00
+Cone's base area: 432.00
+10.0
+30
+"""
+    return 5, input_to_test, expected_output
+
+def testG3():
+
+    input_to_test = """\
+../testcases/file_3.txt"""
+
+    expected_output = """\
+r: 30.00, h: 39.00
+Cone's base area: 2700.00
+9.0
+1
+"""
+    return 6, input_to_test, expected_output
+
+def testG4():
+
+    input_to_test = """\
+../testcases/file_4.txt"""
+
+    expected_output = """\
+r: 3.00, h: 3.00
+Cone's base area: 27.00
+9.0
+100
+"""
+    return 7, input_to_test, expected_output
+
+def testG5():
+
+    input_to_test = """\
+../testcases/file_5.txt"""
+
+    expected_output = """\
+r: 29.00, h: 17.00
+Cone's base area: 2523.00
+6.0
+2
+"""
+    return 8, input_to_test, expected_output
+
+if __name__ == "__main__":
+
+    executable_file = "./Main.py"
+
+    runtest(executable_file, test1())
+    runtest(executable_file, test2())
+    runtest(executable_file, test3())
