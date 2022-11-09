65ef5e635ca67bef8139495bb55d4de0813f27a2
Furkan Akkurt <71407287+furkanakkurt1335@users.noreply.github.com>
2022-11-09 09:46:38 +0300

    update with files given by Murat Hoca

--- 11_9-14_ws5/istanbulkart/src/Main.py
+++ 11_9-14_ws5/istanbulkart/src/Main.py
@@ -13,46 +13,39 @@ day_count = int(f.readline())
 
 # DO_NOT_EDIT_ANYTHING_BELOW_THIS_LINE
 
-# You can create the price variables below (e.g. student first payment is 3.74)
+# You can create the price variables below (e.g. student first payment is 3.74) if you need to
+# You can set your price variables if the card type is student or worker here.
+# (e.g. first payment is 3.74 for student, 7.67 for worker)
+
+# You can define a function that returns True if the card type is 'Student' here ; return False otherwise
+# Also define the function calculate_saved_amount here. This function returns the saved amount (positive)
+
 # DO_NOT_EDIT_ANYTHING_ABOVE_THIS_LINE
 
 student_first = 3.74
 student_second = 1.64
 student_third = 1.54
 student_monthly = 109
+
 worker_first = 7.67
 worker_second = 5.49
 worker_third = 4.17
 worker_monthly = 602
 
-# DO_NOT_EDIT_ANYTHING_BELOW_THIS_LINE
-
-# You can return True if the card type is 'Student' here ; return False if not
 def is_student(card_type):
-# DO_NOT_EDIT_ANYTHING_ABOVE_THIS_LINE
     if card_type == 'Student':
         return True
     else:
         return False
 
-# DO_NOT_EDIT_ANYTHING_BELOW_THIS_LINE
+def calculate_saved_amount(individual_payment, monthly):
+    return abs(individual_payment - monthly)
 
-# You can store your price variables if the card type is student here. (e.g. first payment is 3.74)
 if is_student(card_type):
-# DO_NOT_EDIT_ANYTHING_ABOVE_THIS_LINE
     first, second, third, monthly = student_first, student_second, student_third, student_monthly
 
-# DO_NOT_EDIT_ANYTHING_BELOW_THIS_LINE
-# You can store your price variables if the card type is worker here. (first payment is 7.67)
 else:
-# DO_NOT_EDIT_ANYTHING_ABOVE_THIS_LINE
     first, second, third, monthly = worker_first, worker_second, worker_third, worker_monthly
-# DO_NOT_EDIT_ANYTHING_BELOW_THIS_LINE
-
-# Create the function calculate_saved_amount here. This function returns the saved amount (positive)
-def calculate_saved_amount(individual_payment, monthly):
-# DO_NOT_EDIT_ANYTHING_ABOVE_THIS_LINE
-    return abs(individual_payment - monthly)
 
 # DO_NOT_EDIT_ANYTHING_BELOW_THIS_LINE
 
@@ -60,18 +53,30 @@ def calculate_saved_amount(individual_payment, monthly):
 # If the monthly service is cheaper, print 'Monthly service'
 # Or if the pay-as-you-go system is cheaper, print 'Pay-as-you-go system'
 # You are requested to print 3 times (one for 1 vehicle, one for 2 and one for 3) in this part
-for i in range(1, 4):
+
+
 # DO_NOT_EDIT_ANYTHING_ABOVE_THIS_LINE
+for i in range(1, 4):
+
     if i == 1:
         individual_payment = (day_count*first)*2
+
     elif i == 2:
         individual_payment = (day_count*(first+second))*2
+
     else:
         individual_payment = (day_count*(first+second+third))*2
+
     if individual_payment < monthly:
         print('Pay-as-you-go system')
+
     else:
         print('Monthly service')
+
 # DO_NOT_EDIT_ANYTHING_BELOW_THIS_LINE
+
     # We print the saved amount, whatever the choice is (monthly or pay-as-you-go)
+    # You are expected to define a for loop in the section above,
+    # so the following line is indented intentionally to be executed within the for loop body.
+
     print("%.2f" % (calculate_saved_amount(individual_payment, monthly)))
--- 11_9-14_ws5/tokyometro/src/Main.py
+++ 11_9-14_ws5/tokyometro/src/Main.py
@@ -4,6 +4,7 @@
 #   our_string = ' String with new line \n'
 #   string_stripped = our_string.strip()
 # our_string is now 'String with new line'.
+
 # DO_NOT_EDIT_ANYTHING_ABOVE_THIS_LINE
 
 filename = input()
@@ -14,44 +15,34 @@ day_count = int(f.readline())
 # DO_NOT_EDIT_ANYTHING_BELOW_THIS_LINE
 
 # You can create the price variables below (e.g. student first payment is 750)
+# You can define a function to return True if the card type is 'Student' here ; return False otherwise
+# You can store your price variables if the card type is student or worker here. (e.g. first payment is 750 for students and 1500 for workers)
+# Also define the function calculate_saved_amount here. This function returns the saved amount (positive)
+
 # DO_NOT_EDIT_ANYTHING_ABOVE_THIS_LINE
 
 student_first = 750
 student_second = 600
 student_third = 400
 student_monthly = 17300
+
 worker_first = 1500
 worker_second = 1200
 worker_third = 800
 worker_monthly = 49310
 
-# DO_NOT_EDIT_ANYTHING_BELOW_THIS_LINE
-
-# You can return True if the card type is 'Student' here ; return False if not
 def is_student(card_type):
-# DO_NOT_EDIT_ANYTHING_ABOVE_THIS_LINE
     if card_type == 'Student':
         return True
     else:
         return False
 
-# DO_NOT_EDIT_ANYTHING_BELOW_THIS_LINE
-
-# You can store your price variables if the card type is student here. (e.g. first payment is 750)
 if is_student(card_type):
-# DO_NOT_EDIT_ANYTHING_ABOVE_THIS_LINE
     first, second, third, monthly = student_first, student_second, student_third, student_monthly
-
-# DO_NOT_EDIT_ANYTHING_BELOW_THIS_LINE
-# You can store your price variables if the card type is worker here. (first payment is 1500)
 else:
-# DO_NOT_EDIT_ANYTHING_ABOVE_THIS_LINE
     first, second, third, monthly = worker_first, worker_second, worker_third, worker_monthly
-# DO_NOT_EDIT_ANYTHING_BELOW_THIS_LINE
 
-# Create the function calculate_saved_amount here. This function returns the saved amount (positive)
 def calculate_saved_amount(individual_payment, monthly):
-# DO_NOT_EDIT_ANYTHING_ABOVE_THIS_LINE
     return abs(individual_payment - monthly)
 
 # DO_NOT_EDIT_ANYTHING_BELOW_THIS_LINE
@@ -60,18 +51,28 @@ def calculate_saved_amount(individual_payment, monthly):
 # If the monthly service is cheaper, print 'Monthly service'
 # Or if the pay-as-you-go system is cheaper, print 'Pay-as-you-go system'
 # You are requested to print 3 times (one for 1 vehicle, one for 2 and one for 3) in this part
-for i in range(1, 4):
+
 # DO_NOT_EDIT_ANYTHING_ABOVE_THIS_LINE
+
+for i in range(1, 4):
+
     if i == 1:
         individual_payment = (day_count*first)*2
+
     elif i == 2:
         individual_payment = (day_count*(first+second))*2
+
     else:
         individual_payment = (day_count*(first+second+third))*2
+
     if individual_payment < monthly:
         print('Pay-as-you-go system')
+
     else:
         print('Monthly service')
+
 # DO_NOT_EDIT_ANYTHING_BELOW_THIS_LINE
     # We print the saved amount, whatever the choice is (monthly or pay-as-you-go)
+    # You are expected to define a for loop in the section above
+    # so the following line is indented intentionally to be executed within the for loop body
     print("%.2f" % (calculate_saved_amount(individual_payment, monthly)))
