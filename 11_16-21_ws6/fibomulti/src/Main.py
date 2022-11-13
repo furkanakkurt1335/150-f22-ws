9a7c52ee102f13d0ad869d16a519d01581b5cff6
Furkan Akkurt <71407287+furkanakkurt1335@users.noreply.github.com>
2022-11-14 00:40:01 +0300

    update descriptions

--- 11_16-21_ws6/fibomulti/src/Main.py
+++ 11_16-21_ws6/fibomulti/src/Main.py
@@ -6,11 +6,11 @@ f = open(filename, 'r')
 n = int(f.readline())
 
 # DO_NOT_EDIT_ANYTHING_BELOW_THIS_LINE
+# The function you're going to complete should return the product of the two previous results, using recursion.
+# It's like fibonacci, but we use multiplication instead of addition. The first value is 2 and the second value is 3. Ignore the count variable.
 def fibomulti(number):
     global count
     count += 1
-#The function youre going to complete should return the product of the two previous result using recursion.
-#It's like fibonacci, but we use multiplication instead of addition. first value 2, second value 3. Ignore the count val
 # DO_NOT_EDIT_ANYTHING_ABOVE_THIS_LINE
     if number == 1:
         return 2
