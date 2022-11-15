83cc1630ae3540115ccd486f8c7d24fc6aa50ba3
emresin <Emresin75@outlook.com>
2022-11-15 12:18:55 +0300

    question 'phobicnacci' is added

---
+++ 11_16-21_ws6/soru1/src/Main.py
@@ -0,0 +1,40 @@
+# Read the file
+# DO_NOT_EDIT_ANYTHING_ABOVE_THIS_LINE
+
+filename = input()
+f = open(filename, 'r')
+s1 = int(f.readline())
+s2 = int(f.readline())
+n = int(f.readline())
+
+# DO_NOT_EDIT_ANYTHING_BELOW_THIS_LINE
+def bucci(number):
+    global count
+    count += 1
+# DO_NOT_EDIT_ANYTHING_ABOVE_THIS_LINE
+    if number == 1:
+        return s1
+    elif number == 2:
+        return s2
+    else:
+        first = bucci(number-1)
+        second = bucci(number-2)
+        if(first % 2 == 0 and second % 2 == 0):
+            return int((first/2)) + (2*second+1)
+        else:
+            return first + second
+
+
+# DO_NOT_EDIT_ANYTHING_BELOW_THIS_LINE
+
+count = 0
+
+# You need to call the `bucci` function below
+# DO_NOT_EDIT_ANYTHING_ABOVE_THIS_LINE
+
+res = bucci(n)
+print('Result:', res)
+
+# DO_NOT_EDIT_ANYTHING_BELOW_THIS_LINE
+
+print('Count:', count)
