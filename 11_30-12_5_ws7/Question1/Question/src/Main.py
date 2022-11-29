6c23a5d8da389c8ffb218b9c17ce5ff1f4aef52a
ksezgin <110621312+ksezgin@users.noreply.github.com>
2022-11-30 00:33:44 +0300

    Add files via upload

---
+++ 11_30-12_5_ws7/Question1/Question/src/Main.py
@@ -0,0 +1,34 @@
+import random
+
+seed = int(input())
+random.seed(seed)
+
+# DO_NOT_EDIT_ANYTHING_ABOVE_THIS_LINE
+
+first_number = int(input())
+second_number = int(input())
+
+if (first_number < 0 or first_number > second_number):
+    print("INVALID INPUT")
+    exit()
+
+repeat = random.randint(5, 30)
+
+print(first_number)
+print(second_number)
+
+for _ in range(repeat):
+
+    if _ % 3 == 0:
+        new_number = first_number ** second_number
+
+    elif _ % 3 == 1:
+        new_number = first_number // second_number
+
+    elif _ % 3 == 2:
+        new_number = first_number * second_number
+
+    first_number, second_number = second_number, new_number
+    print(new_number)
+
+# DO_NOT_EDIT_ANYTHING_BELOW_THIS_LINE
