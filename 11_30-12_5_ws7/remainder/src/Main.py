72b353e1679a126b71102ebdba6888705b257191
Furkan Akkurt <71407287+furkanakkurt1335@users.noreply.github.com>
2022-11-30 17:53:59 +0300

    add question by Kaan, named 'remainder'

---
+++ 11_30-12_5_ws7/remainder/src/Main.py
@@ -0,0 +1,36 @@
+import random
+
+filename = input()
+f = open(filename, 'r')
+seed = int(f.readline())
+random.seed(seed)
+
+# DO_NOT_EDIT_ANYTHING_ABOVE_THIS_LINE
+
+first_number = int(f.readline())
+second_number = int(f.readline())
+
+if (first_number < 0 or first_number > second_number):
+    print("INVALID INPUT")
+    exit()
+
+repeat = random.randint(5, 10)
+
+print(first_number)
+print(second_number)
+
+for _ in range(repeat):
+
+    if _ % 3 == 0:
+        new_number = 4 * first_number + second_number
+
+    elif _ % 3 == 1:
+        new_number = (7 * first_number + second_number) % 1000
+
+    elif _ % 3 == 2:
+        new_number = 3 * first_number + second_number
+
+    first_number, second_number = second_number, new_number
+    print(new_number)
+
+# DO_NOT_EDIT_ANYTHING_BELOW_THIS_LINE
